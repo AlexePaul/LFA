9673b7c241ccaa3fcd932159674dc6b072f3c248
AlexePaul <64796131+AlexePaul@users.noreply.github.com>
2022-04-12 13:14:02 +0300

    Final Commit

--- main.cpp
+++ main.cpp
@@ -9,58 +9,58 @@ using namespace std;
 
 ifstream fin("input.txt");
 
-int vertices; // folosit pentru numarul de noduri
-vector< pair<int,char> > edges[500];
-vector<int> finalNodes;
-bool accepted = false;
-string word;
-int position = 0; // it is used for the position in the word.
-vector<int> path;
-int wordCount;
+int edgesCount; // Numarul de Muchii 
+int finalNodesCount; // Numarul de stari finale
+int startingNode; // Starea initiala
+int vertices; // Folosit pentru numarul de noduri
+vector< pair<int,char> > edges[500]; // Lista de adiacenta
+vector<int> finalNodes; // Un Vector in care tin nodurile finale
+bool accepted = false; 
+string word; // Un string pentru cuvantul citit
+int position = 0; // folosit pentru pozitia in cuvant
+vector<int> path; // Vector pentru ruta gasita
+int wordCount; // numarul de cuvinte ce trebuie prelucrate
 
 void dfs(int nod){
-	if(count(finalNodes.begin(),finalNodes.end(),nod) && position >= word.length()){
+	if(count(finalNodes.begin(),finalNodes.end(),nod) && position >= word.length()){ 
+		// Daca cuvantul se afla printre nodurile finale si l-am parcurs pe tot acesta este acceptat 
 		accepted = true;
 		return;
 	}
 	else if(position >= word.length()){
+		// Daca am ajuns la final dar nu este printre nodurile finale acesta nu este acceptat
 		return;
 	}
-	bool entered = false;
-	for(int i = 0; i < edges[nod].size(); ++i){
-		if(edges[nod][i].second == word[position]){
-			position++;
-			path.push_back(edges[nod][i].first);
-			dfs(edges[nod][i].first);
-			if(accepted == true)
+	for(int i = 0; i < edges[nod].size(); ++i){ // parcurg toate muchiile care pleaca din nodul pe care sunt eu acum
+		if(edges[nod][i].second == word[position]){ //verific sa fie aceeasi litera pe muchie ca si cea din cuvant unde am ajuns
+			position++; //trec pe pozitia urmatoare in cuvant
+			path.push_back(edges[nod][i].first); // adaug nodul in ruta
+			dfs(edges[nod][i].first); // apelez recursiv dfs pentru urmatorul nod
+			if(accepted == true) // pentru a nu mai verifica dupa ce deja este acceptat, ies din toate instantele functiei
 				return;
-			path.pop_back();
-			position--;
-			entered = true;
+			path.pop_back(); // scot nodul din ruta daca nu ies din instanta
+			position--; // ma intorc pe pozitia anterioara din cuvant
 		}
 	}
 }
 
 int main(){
-	int edgesCount;
-	int finalNodesCount;
-	int startingNode;
 	int x,y;
-	char c; //random variables to read with.
+	char c; // Variabile folosite la citirea muchiilor
 	fin >>  vertices;
 	fin >> edgesCount;
-	for(int i = 0; i < edgesCount; ++i){
+	for(int i = 0; i < edgesCount; ++i){ // Citesc muchiile
 		fin >> x >> y >> c;
-		edges[x].push_back({y,c});
+		edges[x].push_back(make_pair(y,c));
 	}
 	fin >> startingNode;
 	fin >> finalNodesCount;
-	for(int i = 0; i < finalNodesCount; ++i){
+	for(int i = 0; i < finalNodesCount; ++i){ // Citesc starile finale
 		fin >> x;
 		finalNodes.push_back(x);
 	}
 	fin >> wordCount;
-	for(int j = 0; j < wordCount; j++){
+	for(int j = 0; j < wordCount; j++){ // citesc fiecare cuvant si il parcurg
 		fin >> word;
 		cout << '\n';
 		cout << word << '\n';
@@ -78,4 +78,5 @@ int main(){
 		else
 			cout << "cuvantul nu este acceptat\n";
 	}
+	cout << '\n';
 }
